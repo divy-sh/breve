5f49c96aa60cf13c8efeeeec83b648e1ca56e595
divyendu <divyendua@gmail.com>
2025-04-29 03:56:28 -0400

    minor update

--- store/store.go
+++ store/store.go
@@ -1,13 +1,17 @@
 package store
 
-import "github.com/divy-sh/animus/essentias"
+import "errors"
 
-var urlStore = essentias.NewStringEssentia()
+var urlStore = map[string]string{}
 
 func GetRedirectUrl(shortId string) (string, error) {
-	return urlStore.Get(shortId)
+	url, ok := urlStore[shortId]
+	if !ok {
+		return "", errors.New("key not found")
+	}
+	return url, nil
 }
 
 func SetShortUrl(shortId, url string) {
-	urlStore.Set(shortId, url)
+	urlStore[shortId] = url
 }
