6d822b3c8f858cb9a4d34dc707fa2778970f7c55
divyendu <divyendua@gmail.com>
2025-05-04 00:32:22 -0400

    reset

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// GET /homepage
-router.get('/home', async (req, res) => {
-  res.send('It works!');
-});
-
-module.exports = router;
