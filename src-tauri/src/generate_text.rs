b434ce9492fafe590b6ba4b223096bc6c816ddad
divyendu <divyendua@gmail.com>
2025-05-07 06:17:30 -0400

    fix prompt

--- src-tauri/src/generate_text.rs
+++ src-tauri/src/generate_text.rs
@@ -4,7 +4,7 @@ use tauri::{Emitter, Window};
 
 pub fn generate_handler(prompt: String, window: Window) -> Result<(), String> {
     let model = LlamaModel::load_from_file(
-        "models/SmolLM-1.7B-Instruct-v0.2-IQ3_XS.gguf",
+        "models/smollm2-1.7b-instruct-q4_k_m.gguf",
         LlamaParams::default(),
     )
     .map_err(|e| {
@@ -20,7 +20,9 @@ pub fn generate_handler(prompt: String, window: Window) -> Result<(), String> {
         })?;
 
     let formatted_prompt = format!(
-        "<|system|>Do not return any text other than your response to the user.<|user|>\n{}\n<|assistant|>\n",
+        "<|im_start|>system\nYou are a helpful AI assistant named Breve<|im_end|>\n
+        <|im_start|>user\n{}\n<|im_end|>\n
+        <|im_start|>assistant\n",
         prompt
     );
     ctx.advance_context(&formatted_prompt).map_err(|e| {
@@ -44,7 +46,6 @@ pub fn generate_handler(prompt: String, window: Window) -> Result<(), String> {
         if decoded_tokens > max_tokens {
             break;
         }
-        print!("{}", chunk);
         if let Err(e) = window.emit("llm-stream", chunk.clone()) {
             eprintln!("Emit error: {:?}", e);
         }
