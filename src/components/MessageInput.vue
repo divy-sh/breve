cd1ec2bbbb8cc96bd8681205a0909245d8412374
div <divyendua@gmail.com>
2026-01-24 01:43:09 -0800

    refactor MessageInput component for dynamic textarea height adjustment

--- src/components/MessageInput.vue
+++ src/components/MessageInput.vue
@@ -19,12 +19,21 @@ const messageText = ref('')
 // Methods
 function onMessageTextChange(e: any) {
   messageText.value = e.target.value
+  const el = e.target
+
+  el.style.height = 'auto'
+  el.style.height = el.scrollHeight + 'px'
 }
 
 function handleSend() {
   if (messageText.value.trim() && !props.isLoading) {
     emit('send', messageText.value.trim())
     messageText.value = ''
+  }
+
+  const textarea = document.querySelector('textarea')
+  if (textarea) {
+    textarea.style.height = 'auto'
   }
 }
 
@@ -47,20 +56,29 @@ onMounted(() => {
       :disabled="props.isLoading"
       :value="messageText"
       @input="onMessageTextChange"
-    >
-      <template #right>
-        <k-link
-          @click="handleSend"
-          :class="[
-            'px-3 py-2 font-semibold',
-            props.isLoading || !messageText.trim()
-              ? 'opacity-50 pointer-events-none'
-              : 'text-primary'
-          ]"
-        >
-          <i class="pi pi-send"></i>
-        </k-link>
-      </template>
-    </k-messagebar>
+      class="
+        [&_.h-14]:h-auto
+        [&_.h-12]:h-auto
+
+        [&_.flex]:py-2
+        [&_.flex]:items-end
+
+        [&_textarea]:min-h-48px]
+        [&_textarea]:max-h-[120px]
+        [&_textarea]:overflow-y-auto"
+      >
+        <template #right>
+          <k-link
+            @click="handleSend"
+            :class="[
+              'material:px-4 material:py-7 ios:p-4 font-semibold',
+              props.isLoading || !messageText.trim()
+                ? 'opacity-50 pointer-events-none'
+                : 'text-primary'
+            ]">
+            <i class="pi pi-send"></i>
+          </k-link>
+        </template>
+      </k-messagebar>
   </k-block>
 </template>
