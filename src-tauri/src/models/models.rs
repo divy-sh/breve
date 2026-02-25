7057841252451c3e1cbbfa150928d992466303fb
div <divyendua@gmail.com>
2026-02-25 01:46:08 -0800

    clean up unnecessary model fields

--- src-tauri/src/models/models.rs
+++ src-tauri/src/models/models.rs
@@ -5,12 +5,6 @@ pub struct Model {
     pub name: String,
     pub repo: String,
     pub size: f32,
-    pub prefix: String,
-    pub suffix: String,
-    pub eot: String,
-    pub sys: String,
-    pub us: String,
-    pub ast: String,
     pub supports_vision: bool,
     pub params: String,
 }
